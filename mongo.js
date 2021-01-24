366d5c156edf9bdc3bcd53e99575d7e850147ae3
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-01-24 20:38:16 +0000

    Ensure that Person instance can be generated from both front and back-ends

--- mongo.js
+++ mongo.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose')
-
 const url =
   `mongodb+srv://admin:AshNaz1987@cluster0.ouoed.mongodb.net/person-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(
+  url, 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false, 
+    useCreateIndex: true 
+  }
+)
 
 const personSchema = new mongoose.Schema({
   name: String,
