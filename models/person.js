f38065722f6a4d2bd68d942f862e6a0de8382fc4
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-01-28 22:09:46 +0000

    Complete exercise 3.22

--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('Connecting to', url)
@@ -8,28 +10,28 @@ console.log('Connecting to', url)
 mongoose.set('useFindAndModify', false)
 
 mongoose.connect(
-  url, 
-  { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false, 
-    useCreateIndex: true 
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   }
 )
   .then(_result => console.log('Connected to MongoDB'))
   .catch(error => console.log('Error connecting to MongoDB', error.message))
 
 const personSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true, 
-    unique: true 
+  name: {
+    type: String,
+    required: true,
+    unique: true
   },
-  telNo: { 
-    type: String, 
-    required: true, 
+  telNo: {
+    type: String,
+    required: true,
     minlength: 11,
-    unique: false 
+    unique: false
   }
 })
 
